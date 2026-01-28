a19e59d10e77d691db3a29dc3f12df80e57dac1b
jefferson Oliveira <oliveira2112jj@outlook.com>
2026-01-28 11:44:23 -0300

    create icon component and config svgr vite plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { Text } from "./components";
+import { Icon, Text } from "./components";
+import TrashIcon from "./assets/icons/trash.svg?react";
+import SpinnerIcon from "./assets/icons/spinner.svg?react";
 
 export default function App() {
-  return ( 
-    <Text variant="title-lg" className="text-gray-100">Teste</Text>
+  return (
+    <div className="grid gap-3">
+      <Text variant="title-lg" className="text-gray-100">Teste</Text>
+
+      <Icon svg={TrashIcon} size="md" variant="primary" />
+      <Icon svg={SpinnerIcon} animate size="sm" variant="secondary" />
+    </div>
   )
 }
