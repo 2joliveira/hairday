fbf85ad469b14bc190675bf5371af4cf15eb8c9a
Jefferson Oliveira <oliveira2112@outlook.com>
2026-02-10 17:03:04 -0300

    creat list item component

--- src/components/ListCard.tsx
+++ src/components/ListCard.tsx
@@ -1,3 +1,4 @@
+import { ListItem } from "./ListItem";
 import { Icon, Text } from "./ui";
 import { iconVariants } from "./ui/icon";
 
@@ -7,20 +8,38 @@ interface ListcardProps {
   timeInterval: string;
 }
 
+const mockitems = [
+  { id: 0, hour: "09:00", clientName: "Livia Curtis" },
+  { id: 1, hour: "09:00", clientName: "Livia Curtis" },
+  { id: 2, hour: "09:00", clientName: "Livia Curtis" },
+  { id: 3, hour: "09:00", clientName: "Livia Curtis" },
+  { id: 4, hour: "09:00", clientName: "Livia Curtis" },
+  { id: 5, hour: "09:00", clientName: "Livia Curtis" },
+];
+
 export function Listcard({ icon, title, timeInterval }: ListcardProps) {
   return (
     <div className="mt-8 border border-gray-600 rounded-lg">
-      <header className="px-3 py-5 flex items-center border-b-gray-600 justify-between">
-        <span className="flex gap-2 items-center justify-center">
-          <Icon size="md" svg={icon} className={iconVariants({})} />
-          <Text as="p" variant="text-sm" className="text-gray-300 pt-1">{title}</Text>
-        </span>
+      <header className="px-5 py-3 flex items-center justify-between gap-3 border-b border-b-gray-600">
+        <Icon size="md" svg={icon} className={iconVariants({})} />
 
-        <Text as="p" variant="text-sm" className="text-gray-400 pt-1">{timeInterval}</Text>
+        <Text as="p" variant="text-sm" className="flex-1 text-gray-300 pt-1">
+          {title}
+        </Text>
+
+        <Text as="p" variant="text-sm" className="text-gray-400 pt-1">
+          {timeInterval}
+        </Text>
       </header>
 
-      <main>
-        
+      <main className="p-5 gap-1">
+        {mockitems.map((item) => (
+          <ListItem
+            key={item.id}
+            hour={item.hour}
+            clientName={item.clientName}
+          />
+        ))}
       </main>
     </div>
   );
---
+++ src/components/ListItem.tsx
@@ -0,0 +1,18 @@
+import { ButtonIcon, Text } from "./ui";
+
+import TrashIcon from "@/assets/icons/trash.svg?react";
+
+interface ListItemProps {
+  hour: string;
+  clientName: string;
+}
+
+export function ListItem({ hour, clientName }: ListItemProps) {
+  return (
+    <div className="h-8 flex items-center justify-between gap-5 text-gray-200">
+      <Text variant="title-md">{hour}</Text>
+      <Text variant="text-md" className="flex-1">{clientName}</Text>
+      <ButtonIcon icon={TrashIcon} />
+    </div>
+  )
+}
